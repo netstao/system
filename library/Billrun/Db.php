2e52d834191478f526def7ba435e0422327b8745
Ofer Cohen <oc666@netvision.net.il>
2013-04-23 18:26:16 +0300

    merge from merge from S.D.O.C. LTD repositories

--- library/Billrun/Db.php
+++ library/Billrun/Db.php
@@ -30,7 +30,9 @@ class Billrun_Db extends Mongodloid_Db {
 	public function __construct(\MongoDb $db, \Mongodloid_Connection $connection) {
 		parent::__construct($db, $connection);
 		$this->collections = Billrun_Factory::config()->getConfigValue('db.collections', array());
-		MongoCursor::$timeout = Billrun_Factory::config()->getConfigValue('db.timeout', 300000); // default 5 minutes
+		$timeout = Billrun_Factory::config()->getConfigValue('db.timeout', 3600000); // default 60 minutes
+		Billrun_Factory::log()->log('Set database cursor timeout to: ' . $timeout, Zend_Log::INFO);
+		MongoCursor::$timeout = $timeout;
 	}
 
 	/**
@@ -41,7 +43,7 @@ class Billrun_Db extends Mongodloid_Db {
 	static public function getInstance() {
 		$config = Billrun_Factory::config();
 		$conn = Billrun_Connection::getInstance($config->db->host, $config->db->port);
-		return $conn->getDB($config->db->name);
+		return $conn->getDB($config->db->name, $config->db->user, $config->db->password);
 	}
 
 	/**
@@ -55,7 +57,7 @@ class Billrun_Db extends Mongodloid_Db {
 	 * @return array results of the aggregation
 	 */
 	public function simple_aggregate($collection_name, $where, $group, $having) {
-		$collection = self::db()->getCollection($collection_name);
+		$collection = $this->getCollection($collection_name);
 		return $collection->aggregate(array('$match' => $where), array('$group' => $group), array('$match' => $having));
 	}
 
@@ -79,7 +81,7 @@ class Billrun_Db extends Mongodloid_Db {
 
 	/**
 	 * get collections  for the database.
-	 * @param type $name the name of the colleaction to retreive.
+	 * @param type $name the name of the colleaction to retrieve.
 	 * @return type the requested collection
 	 */
 	public function __get($name) {
